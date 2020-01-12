60c38eb3e5a6fe441b3069cbd7a453ee2b283320
Henri <henri.erasmus@hotmail.com>
2020-01-12 20:21:40 +0200

    Updates

---
+++ precache-manifest.e0da322feb0f9d3b0f03cd958c9d38b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859f840d58e0a94dfb9773489141f2b2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7eb4ffe3174d741bb0d1",
+    "url": "/portfolio/static/css/main.1d58e551.chunk.css"
+  },
+  {
+    "revision": "730558cbe3f459e3d8b3",
+    "url": "/portfolio/static/js/2.ec159163.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.ec159163.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7eb4ffe3174d741bb0d1",
+    "url": "/portfolio/static/js/main.1d6b8955.chunk.js"
+  },
+  {
+    "revision": "f5ff1fe24da837620bb5",
+    "url": "/portfolio/static/js/runtime-main.c507fcf5.js"
+  }
+]);
